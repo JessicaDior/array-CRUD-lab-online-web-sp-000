1f08ec9d9a4a45f926717bbef7a27d81ae8d7227
Jc Dior <jessicaclairdior@gmail.com>
2020-08-24 00:45:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,31 +11,25 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["drums", "bass", "guitar", "piano"]
-  array.unshift("element")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["drums", "bass", "guitar", "piano"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array = ["drums", "bass", "guitar", "piano"]
   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["drums", "bass", "guitar", "piano"]
   array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["drums", "bass", "guitar", "piano"]
   array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array = ["drums", "bass", "guitar", "piano"]
   array.last
 end
