9a5efd6b884d8a22258f891f5a715cca774b779a
Jc Dior <jessicaclairdior@gmail.com>
2020-08-24 00:43:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["drums", "bass", "guitar", "piano"]
+  array = ["element", "element", "element", "element"]
   array << "element"
 end
 
